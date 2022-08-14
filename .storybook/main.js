74459302a1d181b247e0872dc836f9958c221318
junkor-1011 <tomoyukizxy@gmail.com>
2022-08-14 21:01:49 +0900

    chore: setup development tools

---
+++ .storybook/main.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  webpackFinal(config) {
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')];
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
+
+    // Sass & CSS Modules
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              auto: true,
+            },
+          },
+        },
+        'sass-loader',
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    return config;
+  },
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+};
