8211706a681ec065daa3861bbd28cb5d42fdaa4b
junkor-1011 <tomoyukizxy@gmail.com>
2022-09-03 00:36:48 +0900

    performance: use esbuild-jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
+  // preset: 'ts-jest/presets/default-esm',
   globals: {
-    'ts-jest': {
-      useESM: true,
+    // 'ts-jest': {
+    //   useESM: true,
+    // },
+    'esbuild-jest': {
+      tsconfig: '<rootDir>/tsconfig.jest.json',
     },
   },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '^@/(.+)': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'esbuild-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+  },
 };
