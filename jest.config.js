f565fdf8628112f0a1dc2affce9d8be2d6e5875f
junkor-1011 <tomoyukizxy@gmail.com>
2022-09-10 23:40:24 +0900

    testing: enable storyshots

--- jest.config.js
+++ jest.config.js
@@ -1,42 +1,49 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // preset: 'ts-jest/presets/default-esm',
   globals: {
-    // 'ts-jest': {
-    //   useESM: true,
-    // },
     '@swc/jest': {
       tsconfig: '<rootDir>/tsconfig.json',
     },
   },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'mdx'],
   moduleNameMapper: {
     '^@/(.+)': '<rootDir>/src/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.(t|j)sx?$': [
       '@swc/jest',
       {
         sourceMaps: true,
-        /*
-      module: {
-        type: 'commonjs',
-      },
-      jsc: {
-        parser: {
-          syntax: 'typescript',
-          tsx: true,
+        module: {
+          type: 'commonjs',
         },
-      transform: {
-          react: {
-            runtime: 'automatic'
-          }
-        }
-      },
-      */
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
       },
     ],
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
---
+++ src/__tests__/snapshot.test.tsx
@@ -0,0 +1,15 @@
+import initStoryshots from '@storybook/addon-storyshots';
+
+initStoryshots();
+
+// import { render } from '@testing-library/react';
+// import Home from  '../pages/index'
+//
+//
+// describe('snapshot test', () => {
+//   it('Home Component', () => {
+//     const { container } = render(<Home />);
+//     // eslint-disable-next-line testing-library/no-node-access
+//     expect(container.firstChild).toMatchSnapshot();
+//   });
+// });
