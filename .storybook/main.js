4a4c487f7cb72ff3dff3d48f9d35306fa2e2114e
junkor-1011 <tomoyukizxy@gmail.com>
2022-09-11 18:55:53 +0900

    feat: emotionjs enable in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,26 @@ const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    // '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  babel: async (options) => {
+    // emotion用の注入設定
+    // preset-react ローダーを取得してくる
+    const presetReact = options.presets.find((p) => /preset-react/.test(p[0]));
+    // preset-react　ローダのオプションを設定
+    presetReact[1] = {
+      ...presetReact[1],
+      runtime: 'automatic',
+      importSource: '@emotion/react',
+    };
+    // Emotionプラグインを追加
+    options.plugins.push(require.resolve('@emotion/babel-plugin'));
+
+    return options;
+  },
   webpackFinal(config) {
     config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')];
     config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
