c361e16bd6a46c5c6b6207ea7f419fa588d70069
junkor-1011 <tomoyukizxy@gmail.com>
2022-09-11 23:46:11 +0900

    fix: broken build of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
-    '@storybook/preset-scss',
+    // '@storybook/preset-scss',
     'storybook-addon-pseudo-states',
   ],
   babel: async (options) => {
